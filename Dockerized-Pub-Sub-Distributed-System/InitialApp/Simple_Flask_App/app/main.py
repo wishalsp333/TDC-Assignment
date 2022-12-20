b4020d74c2811dfade1857f46293dc85794702da
Vishal Patil <wishalsp333@gmail.com>
2022-12-20 00:06:39 -0800

    Initial Commit

---
+++ Dockerized-Pub-Sub-Distributed-System/InitialApp/Simple_Flask_App/app/main.py
@@ -0,0 +1,11 @@
+
+#main.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return " Welcome ! This python flask app is running in linux distro inside Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
